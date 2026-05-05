d3337048f146211a42a547fb5d6d674cecb10fee
pajecawav <pajecawav@gmail.com>
2026-05-05 20:12:59 +0300

    feat: add missing export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { defineCommitlintConfig } from "./tools/commitlint/index.js";
 export { defineLintStagedConfig } from "./tools/lint-staged/index.js";
 export { defineOxfmtConfig } from "./tools/oxfmt/index.js";
+export { defineOxlintConfig } from "./tools/oxlint/index.js";
 export { definePrettierConfig } from "./tools/prettier/index.js";
